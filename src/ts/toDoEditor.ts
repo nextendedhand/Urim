4af62b03bacc96e9e88a6e27a2762b7d0b640991
TauX <divetoblue6@gmail.com>
2019-11-29 02:51:39 +0900

    [modify] modify for extract FormInfoManager class

---
+++ src/ts/toDoEditor.ts
@@ -0,0 +1,25 @@
+import FormInfoManager from './toDoCreator';
+
+window.onload = () => {
+    const fim = new FormInfoManager();
+    fim.expireForm();
+    fim.setPlaceHolder("temp");
+};
+
+let createBtn = document.getElementById("expire_btn");
+createBtn.addEventListener("click", () => {
+    const fim = new FormInfoManager();
+    if (fim.checkInputFilled()) {
+        fim.expireTask(fim.getTaskInfo());
+        location.href = '../html/index.html';
+    } else {
+        // "入力が不足しています"
+    }
+}, false);
+
+let cancelBtn = document.getElementById("cancel_btn");
+cancelBtn.addEventListener("click", () => {
+    location.href = '../html/index.html';
+}, false);
+
+
---
+++ src/ts/toDoRegister.ts
@@ -0,0 +1,24 @@
+import FormInfoManager from './toDoCreator';
+
+window.onload = () => {
+    const fim = new FormInfoManager();
+    fim.expireForm();
+};
+
+let createBtn = document.getElementById("create_btn");
+createBtn.addEventListener("click", () => {
+    const fim = new FormInfoManager();
+    if (fim.checkInputFilled()) {
+        fim.registerTask(fim.getTaskInfo());
+        location.href = '../html/index.html';
+    } else {
+        // "入力が不足しています"
+    }
+}, false);
+
+let cancelBtn = document.getElementById("cancel_btn");
+cancelBtn.addEventListener("click", () => {
+    location.href = '../html/index.html';
+}, false);
+
+
